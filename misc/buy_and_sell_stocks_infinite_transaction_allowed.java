7a02ddf4eccdfb22db60b3d5415ba6918d0bb7fc
weevil111 <68731330+weevil111@users.noreply.github.com>
2021-05-11 13:57:33 +0530

    Create buy_and_sell_stocks_infinite_transaction_allowed.java

---
+++ misc/buy_and_sell_stocks_infinite_transaction_allowed.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i,profit = 0, ans=0;
+		int arr[] = new int[n];
+        for(i = 0; i < n; i++){
+        	arr[i] = sc.nextInt();
+        }
+        boolean transact = true;
+        for(i = 1; i < n; i++){
+        	if(arr[i] < arr[i-1]){
+        		if(transact){
+	        		ans += profit;
+	        		transact = false;
+	        		profit = 0;
+        		}
+        	}else{
+        		profit += arr[i] - arr[i-1];
+        		transact = true;
+        	}
+        }
+        if(profit > 0){
+        	ans += profit;
+        }
+        System.out.println(ans);
+    }
+
+}
