6e0cb7d6c7f55e8566808b618dc41300681be091
weevil111 <68731330+weevil111@users.noreply.github.com>
2021-05-25 08:46:51 +0530

    Create normal_queue.java

---
+++ stack_and_queue/normal_queue.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+  public static class CustomQueue {
+    int[] data;
+    int front;
+    int size;
+
+    public CustomQueue(int cap) {
+      data = new int[cap];
+      front = 0;
+      size = 0;
+    }
+
+    int size() {
+      return size;
+    }
+
+    void display() {
+      for(int i = 0; i < size; i++){
+      	System.out.print(data[(front+i) % data.length]+" ");
+      }
+      System.out.println();
+    }
+
+    void add(int val) {
+      if(size < data.length){
+      	data[(front+size)%data.length] = val;
+      	size++;
+      }else{
+      	System.out.println("Queue overflow");
+      }
+    }
+
+    int remove() {
+      if(size > 0){
+      	int d = data[front];
+      	front = (front + 1) % data.length;
+      	size--;
+      	return d;
+      }else{
+      	System.out.println("Queue underflow");
+      	return -1;
+      }
+    }
+
+    int peek() {
+       if(size > 0){
+       	return data[front];
+       }else{
+       	System.out.println("Queue underflow");
+      	return -1;
+       }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    CustomQueue qu = new CustomQueue(n);
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("add")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if(str.startsWith("remove")){
+        int val = qu.remove();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("peek")){
+        int val = qu.peek();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("size")){
+        System.out.println(qu.size());
+      } else if(str.startsWith("display")){
+        qu.display();
+      }
+      str = br.readLine();
+    }
+  }
+}
